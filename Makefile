df66086a7ae5924f97a4b2b622652395c048c598
Simon TRUXILLO <struxill@e3r13p11.42.fr>
2018-11-08 15:54:36 +0100

    few functions to start my lib CLEANED

--- Makefile
+++ Makefile
@@ -6,25 +6,27 @@
 #    By: struxill <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/11/07 21:48:46 by struxill          #+#    #+#              #
-#    Updated: 2018/11/07 21:52:50 by struxill         ###   ########.fr        #
+#    Updated: 2018/11/08 15:53:57 by struxill         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME	= libft.a
 CFLAGS	= -Wall -Wextra -Werror
-SOURCES	= 
-
-
+SOURCES	= ft_putchar.c ft_putchar2.c ft_putstr.c ft_putstr2.c ft_putnbr.c
+MAIN	= main.c
 
 all:
-	gcc -c $(SOURCES)
-	ar rc *.o $(NAME)
-	ranlib 
+	gcc -c $(CFLAGS) $(SOURCES)
+	ar rc $(NAME) *.o
+	ranlib $(NAME)
 
 clean:
 	rm -f *.o
 
 fclean: clean
-	rm -f $(NAME)
+	rm -f $(NAME) a.out
 
 re: fclean all
+
+main: re
+	gcc $(CFLAGS) $(MAIN) -L. -lft
---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/08 15:09:53 by struxill          #+#    #+#             */
+/*   Updated: 2018/11/08 15:31:00 by struxill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __LIBFT_H
+# define __LIBFT_H
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putchar2(char c);
+void	ft_putstr(char const *s);
+void	ft_putstr2(char const *s);
+void	ft_putnbr(int n);
+
+#endif
