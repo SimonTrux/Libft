3c610d2a2e07ced379727738332f88d945fbcbe6
Simon TRUXILLO <struxill@e2r6p3.42.fr>
2018-11-10 19:39:21 +0100

    update main pointeur sur fonction

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 15:21:56 by struxill          #+#    #+#             */
-/*   Updated: 2018/11/09 18:41:03 by struxill         ###   ########.fr       */
+/*   Updated: 2018/11/10 19:37:27 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,34 @@
 #define YEL   "\x1B[33m"
 #define CYN   "\x1B[36m"
 #define RESET "\x1B[0m"
+#define TEST(x)  ft_putendl(YEL "\nTEST DE "x"\n" RESET)
 
-//int	ft_inttest
+
+int	ft_int_test_int(void (*f1)(int), int *tab)
+{
+	int i;
+
+	i = 0;
+	while (i > 10)
+	{
+		(*f1)(tab[i]);
+		ft_putchar('\n');
+//		(*f2)(tab[i]);
+		i++;
+	}
+	return (0);
+
+
+}
 
 int	main(int ac, char **av)
 {
+	int tabint[10] = { -42, -1, 0, 1, 2, 2147483647, -2147483648, -9999, 987654321, 42 };
 
+//	tabint[10] = 15;
 	//	PUTSTR PUTNBR PUTCHAR
 	//	(void)av;
-	ft_putstr(YEL "TEST PUTSTR PUTNBR PUTCHAR\n" RESET);
+	TEST("PUTSTR PUTCHAR");
 	ft_putchar('\n');
 	ft_putstr("There is ");
 	ft_putnbr(ac - 1);
@@ -34,19 +53,19 @@ int	main(int ac, char **av)
 	if ((ac - 1) > 1)
 		ft_putchar('s');
 	ft_putstr(".\n");
-	ft_putstr("test putnbr : ");
+	TEST("PUTNBR WITH FT_TEST_INT");
+	ft_int_test_int(ft_putnbr, tabint);
 	ft_putnbr(-1555);
 	ft_putchar('\n');
-	ft_putendl("Ceci est un test de ft_putndl");
-	ft_putendl("Ceci est un test de ft_putndl");
+	TEST("PUTENDL");
 	ft_putendl("Ceci est un test de ft_putndl");
 	if (ac > 1)
 	{	
-		ft_putstr(YEL "ATOI TEST from arg 1 : " RESET);
-		ft_putnbr(ft_atoi(av[1]));
+		TEST("ATOI");
+//		ft_int_test_int(ft_atoi, tabint);
 		ft_putchar('\n');
 		ft_putchar('\n');
-		ft_putendl(YEL "ITOA TEST" RESET);
+		TEST("ITOA");
 		ft_putchar('\n');
 		ft_putstr(ft_itoa(987));
 		ft_putchar('\n');
@@ -58,7 +77,7 @@ int	main(int ac, char **av)
 	//	STRCMP
 	if (ac > 2)
 	{
-		ft_putstr("TEST STRCMP");	
+		TEST("STRCMP");
 		ft_putchar('\n');
 		printf("my strcmp de av1 et av2 : %d\n", ft_strcmp(av[1], av[2]));
 		printf("ori strcmp de av1 et av2 : %d\n", strcmp(av[1], av[2]));
